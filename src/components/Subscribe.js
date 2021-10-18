72ed932f3f0949304923bcdf81f9bea5c7e5bbc1
niawjunior <niawkung@gmail.com>
2021-10-18 23:16:10 +0700

    update ui layout

--- src/components/Subscribe.js
+++ src/components/Subscribe.js
@@ -2,25 +2,41 @@ import React, { useState } from "react";
 import TextField from "@mui/material/TextField";
 import Grid from "@mui/material/Grid";
 import Button from "@mui/material/Button";
+import Snackbar from "@mui/material/Snackbar";
+
 import Layout from "./Layout";
 const Subscribe = () => {
   const [email, setEmail] = useState("");
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
 
   const handleSubscribe = () => {
     console.log(email);
+    setSnackbarOpen(true);
   };
 
   const onEmailChange = (email) => {
     setEmail(email);
   };
+
+  const handleCloseSnackbar = () => {
+    setSnackbarOpen(false);
+  };
   return (
     <Layout>
+      <Snackbar
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+        open={snackbarOpen}
+        onClose={handleCloseSnackbar}
+        message="Thank you for subscribing"
+        key={"top-right"}
+        autoHideDuration={2000}
+      />
       <Grid container justifyContent="center">
         <Grid item xs={4} justifyContent="center">
           <TextField
             style={{ marginTop: "1rem" }}
             fullWidth
-            id="outlined-basic"
+            id="subscribe_email_value"
             label="Email"
             variant="outlined"
             value={email}
