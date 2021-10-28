cc6785f598a2144e36e4c23626db218d24b88c8e
niawjunior <niawkung@gmail.com>
2021-10-28 15:46:42 +0700

    update layout

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,16 +2,16 @@ import React from "react";
 import Box from "@mui/material/Box";
 import Layout from "./Layout";
 import GenericHomeLoan from "./GenericHomeLoan";
-import SecondHandHomeLoan from "./SecondHandHomeLoan";
-import HomeRefinance from "./HomeRefinance";
+// import SecondHandHomeLoan from "./SecondHandHomeLoan";
+// import HomeRefinance from "./HomeRefinance";
 import "../App.css";
 function Home() {
   return (
     <Box style={{ minHeight: "100vh" }} sx={{ flexGrow: 1 }}>
       <Layout>
         <GenericHomeLoan />
-        <SecondHandHomeLoan />
-        <HomeRefinance />
+        {/* <SecondHandHomeLoan /> */}
+        {/* <HomeRefinance /> */}
       </Layout>
     </Box>
   );
