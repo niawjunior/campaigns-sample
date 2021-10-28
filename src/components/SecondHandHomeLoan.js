a589290d91849a58209efa0bd0ad78c393594e42
niawjunior <niawkung@gmail.com>
2021-10-28 16:58:17 +0700

    update route

--- src/components/SecondHandHomeLoan.js
+++ src/components/SecondHandHomeLoan.js
@@ -31,7 +31,6 @@ const SecondHandHomeLoan = () => {
               <button
                 className="apply-button content-text-4"
                 id="second-hand-home-loan-content-text-4"
-                data-evg-clickthrough
               >
                 Apply Online
               </button>
